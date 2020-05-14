43dc72f16436b3d745442aea9f264d5eb0db77b9
Dalton Maahs <maahs2017@gmail.com>
2020-05-13 19:54:06 -0500

    ai-joe

--- D_bot.py
+++ D_bot.py
@@ -1,11 +1,27 @@
 import discord
 import parser
+from random import randint
+
+CLIENTID = 710029944646008883
+PERMISSIONINT = 67584
+
 print(discord.__version__)
+print("This invite link for your bot is: " +
+      f"https://discordapp.com/oauth2/authorize?client_id={CLIENTID}&scope=bot&permissions={PERMISSIONINT}")
+
 
 token = open("token.txt", "r").read()
 
 client = discord.Client()
 
+def quote(person):
+    quotes = open(person + "_quotes.txt").read()
+    qs = []
+    for q in filter(lambda l: "====================" != l, quotes.splitlines()):
+        qs.append(q.replace('[', '').replace(']', '').replace("'", ''))
+
+    return "> " + qs[randint(0, len(qs) - 1)] + '\n- ai-' + person
+    
 @client.event
 async def on_ready():
     print(f'We have loggin in as {client.user}')
@@ -13,7 +29,12 @@ async def on_ready():
 @client.event
 async def on_message(message):
     print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
-    if 'hi there' in message.content.lower():
-        await message.channel.send("HI!")
+    botargs = message.content.lower().split(' ')
+    if 'dbot.quote' == botargs[0]:
+        if len(botargs) == 2:
+            await message.channel.send(quote(botargs[1]))
+        else:
+            await message.channel.send(quote("joe"))
+        
 
 client.run(token)
