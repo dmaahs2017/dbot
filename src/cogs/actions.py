f82e68bb9bf78f5b40c6bed6c9b1542dc067c700
Dalton Maahs <maahs2017@gmail.com>
2020-05-25 00:34:32 -0500

    almost

--- src/cogs/actions.py
+++ src/cogs/actions.py
@@ -2,6 +2,8 @@ import discord
 import os
 from discord.ext import commands
 import random
+import requests
+import json
 
 
 class Actions(commands.Cog):
@@ -11,6 +13,38 @@ class Actions(commands.Cog):
         self._last_member = None
 
     # COMMANDS
+
+    @commands.command(help="Description: Generates a strawpoll")
+    async def strawpoll(self, ctx, title, description, *answers):
+        body = {
+            "poll": {
+                "title": title,
+                "description": description,
+                "answers": [],
+                "priv": 0,
+                "ma": 0,
+                "captcha": False,
+                "mip": 0,
+                "co": 1,
+                "vpn": True,
+                "enter_name": 0,
+                "has_deadline": False,
+                "deadline": None,
+                "only_reg": 0,
+                "has_image": 0,
+                "image": None,
+                "show_results": 1
+            }
+        }
+        for answer in answers:
+            body["poll"]["answers"].append(answer)
+        
+        response = requests.post(url="https://strawpoll.com/api/poll", json=json.dumps(body))
+        print("Status: " + str(response.status_code))
+        print(response.text)
+        print("is redirect?: " + str(response.is_redirect))
+        print(response.request.body)
+        await ctx.send(response.text)
 
     @commands.command(help="Description: Gets random ig post from an ig account")
     async def ig(self, ctx, acct):
